e6de81e440dad113f5912fc86e94f31d3bc01ee5
dani <robotjefe@gmail.com>
2017-09-18 11:42:15 +0200

    plugin

---
+++ hello.php
@@ -0,0 +1,176 @@
+<?php
+
+/*
+Plugin Name: Hello Pratchett
+Plugin URI: 
+Description: Esto es solo un pluging igual que el Helo Dolly pero con frases de Terry Pratchett
+Author: Daniel Sánchez
+Version: 1.0
+Author URI: https://yosydani.com/
+*/
+
+function hello_Pratchett_get_quotes() {
+	/** Frases de Terry Prattchet */
+	$quotes ="A los dioses no les gusta que las personas no trabajen mucho. Las personas que no están ocupadas continuamente pueden empezar a pensar.
+La estupidez real siempre vence a la inteligencia artificial.Las posibilidades de una entre un millón salen bien nueve de cada diez veces.
+Solamente en los sueños somos libres. El resto del tiempo necesitamos el sueldo.
+La lógica es maravillosa, pero a veces obtienes mejores resultados pensando.
+Descubre dónde está el dinero y ya tienes la mitad del problema resuelto.
+La vida habita en todos los lugares donde puede. Donde no puede, simplemente tarda un poco más.
+Tú eres solo una coma en las páginas de esta historia.
+La presencia de los que buscan la verdad es infinitamente preferida a la presencia de aquellos que piensan que la han encontrado.
+La verdad quizá esté ahí fuera pero las mentiras están en tu cabeza.
+Prefiero ser un simio alzado que un ángel caído.
+No vale la pena hacer algo a no ser que alguien, en algún lugar, prefiriera que no lo hicieras.
+Las historias con imaginación suelen cabrear a aquellos que no la tienen.
+La imaginación, no la inteligencia, es lo que nos hace humanos.
+La historia tiene la costumbre de cambiar a las personas que se creen que la están cambiando a ella.
+Ankh-Morpork había coqueteado con muchas formas de gobierno, y había terminado asumiendo ese tipo de democracia que se conoce como ‘Un Hombre, Un Voto’. El Patricio era el Hombre, y el Voto era el suyo.
+Hay veces en la vida en los que la gente debe saber cuando no dejar marchar. Los globos están diseñados para enseñar esto a los niños pequeños.
+Hay un rumor que dice que he encontrado a Dios. Creo que no es cierto porque ya me es difícil encontrar mis llaves, y hay evidencia empírica de que estas existen.
+El problema de tener una mente abierta es que la gente insiste en entrar dentro y poner allí sus cosas.
+Si no conviertes tu vida en una historia, te vuelves parte de la historia de otro.
+Llovía tan fuerte que las gotas tenían que hacer cola.
+Cuando mueres lo primero que pierdes es la vida. Después pierdes tus ilusiones.
+No importa lo rápido que viaje la luz, siempre se encuentra con que la oscuridad ha llegado antes y la está esperando.
+La pluma es más poderosa que la espada, si la espada está envainada y la pluma muy afilada.
+La bondad es sobre lo que haces. No a quién rezas.
+El universo entero ha sido dividido cuidadosamente entre cosas para (a) emparejarse (b) comer, (c) huir, y (d) piedras.
+A veces es mejor encender un lanzallamas que maldecir a la oscuridad.
+La Esfinge es una criatura irreal, y existe únicamente porque ha sido imaginada.
+La Muerte no es cruel… Sencillamente, su trabajo se le da muy bien.
+La mayoría de la gente vive su vida como una especie de borrón en torno al punto donde se encuentra su cuerpo,anticipándose al futuro o aferrándose al pasado. Suelen estar tan preocupados con lo que sucederá que sólo averiguan lo que sucede cuando ya ha sucedido. Así son la mayor parte de las personas. Aprenden a tener miedo porque no saben lo que va a suceder. Y ya les está sucediendo.
+Tenemos mucha experiencia en no tener experiencia. Pero el hecho es… el hecho es… el hecho es que nosotros no hemos tenido experiencia durante mucho más tiempo.
+Los cementerios están llenos de gente que fue más valiente que sensata.
+Dicen que un poco de conocimiento es peligroso, pero no tanto como mucha ignorancia.
+La gravedad es una costumbre difícil de olvidar.
+El conjunto de la vida es como ver una película. Solo es como si siempre llegases diez minutos después de que empezara y nadie te contase el argumento, así que tienes que averiguarlo a través de las pistas.
+El tiempo es una droga, en cantidades excesivas, mata.
+Los demonios eran como genios, o como profesores de filosofía: si no formulabas la pregunta con toda precisión, les encantaba darte respuestas perfectamente precisas y falsas.
+No se puede ir por ahí construyendo un mundo mejor para la gente. Sólo la gente puede construir un mundo mejor para la gente. Si no, es solamente una jaula.
+Emplear el sarcasmo con según qué gente es como atacar un castillo con merengues.
+Nunca confíes en una especie que sonríe todo el tiempo. Está tramando algo.
+Más pronto o más tarde todos somos el perro de alguien.
+Para comprender toda religión es imprescindible saber que a los Dioses les divierte ver a las niñas saltando a la comba con alambres de púas, juegan a cosas que no son el ajedrez con los destinos de los mortales y los tronos de los Reyes. Es importante recordar que siempre hacen trampas, del principio al final.
+No le mareaba la visión de la sangre, pero no soportaba ver la suya propia.
+Una mentira puede dar la vuelta al mundo antes de que la verdad tenga tiempo a ponerse las botas.
+Dale fuego a un hombre y estará caliente un día, pero préndele fuego y estará caliente el resto de su vida.
+Los impuestos son sólo una manera sofisticada de exigir dinero con amenazas.
+Probablemente el último sonido antes de que el Universo se repliegue sobre sí mismo, será alguien diciendo: “¿Qué ocurre si aprieto esto?.”
+Un pie en el cuello es el noventa por ciento de la ley.
+Escribir es lo más divertido que se puede hacer sin ayuda.
+Y todos en la mesa se indignaron, todos miraron ofendidos a los demás, todos pensaron “esto es injusto, alguien tiene que decir algo”. Todos callaron y al cabo de un rato, todos pensaron a la vez ¡cobardes!
+No me asusta la caída, lo que me asusta es el aterrizaje…
+Un burro puede hacer de buey en las épocas en que no hay caballos.
+Volver donde empezaste no es lo mismo que nunca haber marchado.
+Es todavía magia si sabes cómo se hace.
+Los Padres nunca fueron jóvenes. Sólo estaban esperando a convertirse en Padres.
+La mayoría de los dioses juegan a los dados, pero el Destino juega al ajedrez, y sólo te das cuenta una vez que es demasiado tarde de que lleva jugando toda la partida con dos reinas.
+Los niños de verdad no van dando saltitos a menos que hayan tomado drogas.
+No hay nada mejor para conseguir algo que el no saber que es imposible.
+En el interior de cada anciano hay un joven preguntándose qué demonios ha pasado.
+Cinco exclamaciones. El signo seguro de una mente insana.
+El caos siempre derrota al orden porque está mejor organizado.
+El vudú es una religión muy interesante para toda la familia, incluso para los miembros que están muertos.
+Las mascotas son siempre de gran ayuda en tiempos de estrés. Y en tiempos de hambruna también, por supuesto.
+Dios se rige por patrones extremadamente misteriosos, por no decir tortuosos. El asunto de los dinosaurios es un chiste que los paleontólogos no acaban de coger.
+Te puedes comunicar con los muertos tecleando en un ZX Spectrum.
+La fantasía es una bicicleta estática para la mente. Puede no llevarte a ninguna parte, pero tonifica los músculos que puede.
+Las buenas excusas abren la puerta a las malas excusas.
+Aquella gente apenas tenía nada, salvo montones de oro.
+El sexo es como la cocina: a veces te compras libros enteros llenos de recetas complicadas y fotos interesantes y en ocasiones cuando tienes mucha hambre te imaginanas unos banquetes de impresión. Sin embargo, al final del día te conformas felizmente con unos huevos con chorizo, siempre que estén bien preparados y tal vez acompañados por unas buenas patatas fritas…
+La vida es un hábito, resulta muy difícil dejarlo…
+Lo que necesitan los dioses es que crean en ellos, y lo que quieren los humanos es dioses.
+Yo pensaba que las espadas tenían que ser rectas… Quizá al principio son rectas, y luego se van doblando con el uso.
+Algunos piensan que es pura paranoia. Nada de eso. Un paranoico cree que todo el mundo se la tiene jurada. Yo lo sabe.
+Es un animal. Los animales no pueden asesinar. Solo nosotras las razas superiores podemos asesinar. Esa es una de las cosas que nos distinguen de los animales.
+La unidad de tiempo más corta del multiverso es el Segundo de Nueva York, que se define como el período de tiempo que hay entre cuando el semáforo se pone en verde y cuando el taxi que llevas detrás toca el claxon.
+Conozco a las personas que hablan de sufrir por el bien común. ¡Nunca son ellos, joder! Cuando oyes a un hombre gritar: ¡Adelante, bravos camaradas!, verás que siempre es el que está detrás de la jodida roca enorme, y el único que lleva el casco realmente a prueba de flechas.
+El numero de águilas capaces de alzar el vuelo cargando con un toro , se puede contar con los dedos de una culebra.
+La fe es una de las fuerzas orgánicas más poderosas, puede que no sea capaz de mover montañas, al menos en el sentido literal, pero puede crear a alguien que sí sea capaz de hacerlo.
+La fe es una fuerza. Comparada con la gravedad es una fuerza débil, por supuesto, y cuando se trata de mover montañas la gravedad siempre acaba ganando; pero aun así existe.
+Sólo por que no sea agradable no significa que no sea milagroso.
+Cuando puedes aplastar ciudades enteras a voluntad, la tendencia a reflexionar en silencio y ver las cosas desde el punto de vista del otro, rara vez resulta necesaria.
+La lógica sólo es una manera ordenada de alcanzar la ignorancia.
+Tirar juntos es el objetivo del despotismo y de la tiranía. Los hombres libres tiran en toda clase de direcciones.
+Los hombres deberían morir por las mentiras. Pero la verdad es demasiado preciosa para morir por ella.
+Por supuesto que voy a morir. De eso es de lo que se trata estar vivo.
+Recuerda siempre que la multitud que aplaude tu coronación es la misma que aplaudirá tu decapitación. A la gente le gustan los espectáculos.
+Cuando se es un dios, no es preciso aportar argumentos.
+Se dice que lo contrario del ruido es el silencio. Mentira. El silencio no es más que la ausencia de ruido.
+A grandes rasgos, la única habilidad que los alquimistas de Ankh-Morpork habían descubierto hasta el momento era la capacidad de convertir oro en menos oro.
+Era el tipo de ladrón capaz de robarte la iniciativa en el último momento y las palabras de la boca.
+No se puede hacer a la gente feliz por ley.
+Sólo porque alguien sea miembro de una minoría étnica no significa que no sea un pequeño cabrón desagradable.
+Los Dioses tienen tendencia a hacer exactamente el mismo tipo de cosas que harían los hombres si pudieran, sobre todo en los asuntos relativos a las ninfas acuáticas, las lluvias de oro y la exterminación de los enemigos.
+El tiempo es una mera costumbre.
+Y, aunque se consideraba una prueba bastante buena de criminalidad estar viviendo en una chabola, por alguna razón la posesión de una calle entera llena de ellas simplemente hacía que te invitaran a las mejores fiestas sociales.
+Cualquier músico, en algún momento de su vida, tiene que empeñar su instrumento si quiere cenar caliente y dormir a cubierto.
+Los enanos suelen temer las alturas, dado que no disponen de muchas oportunidades de acostumbrarse a ellas.
+Es difícil tener fé, ¿verdad?, cuando se leen demasiados libros.
+La vida de una persona sí pasa delante de sus ojos antes de morir. El proceso se llama 'Vida'.
+Soy la Muerte, no los impuestos, yo aparezco solamente una vez."
+;
+
+	// Separo el string en lineas y lo incluyo en un array
+	$quotes = explode( "\n", $quotes );
+
+	// selecciono una frase aleatoriamente
+	return wptexturize( $quotes[ mt_rand( 0, count( $quotes ) - 1 ) ] );
+}
+
+// echo la frase selecionada
+function hello_pratchett() {
+	$chosen = hello_Pratchett_get_quotes();
+	echo "<div class='notif peek'  id='pratchett' > <p>$chosen</p></div>";
+}
+
+// añado el hook para que se ejecuate cuando se muestren las notificaciones del administrador
+add_action( 'admin_notices', 'hello_pratchett' );
+
+// Un poco de css para posicionar
+function pratchett_css() {
+	echo "
+	<style type='text/css'>
+		.notif {
+		  position: fixed;
+		  right: 20px;
+		  bottom: -150px;
+		  background-color: #323232;
+		  padding: 12px 24px 17px 24px;
+		  vertical-align: middle;
+		  color: #fff;
+		  box-shadow: 0 7px 18px rgba(0,0,0,0.2);
+		  border-radius: 1px;
+		  font-family: 'Trebuchet MS', Helvetica, sans-serif;
+		  z-index: 1;
+		}
+
+		.peek {
+		  -webkit-animation: peek 10s forwards;
+		  -webkit-animation-delay: 0.5s;
+		  animation: peek 10s forwards;
+		  animation-delay: 500ms;
+		}
+
+		@-webkit-keyframes peek {
+		      0% { bottom: -150px; }
+		     10% { bottom: 80px; }  /*demo purposes; change to 20px*/
+		     90% { bottom: 80px; }  /*demo purposes; change to 20px*/
+		    100% { bottom: -150px; }
+		}
+
+		@keyframes peek {
+		      0% { bottom: -150px; }
+		     10% { bottom: 80px; }  /*demo purposes; change to 20px*/
+		     90% { bottom: 80px; }  /*demo purposes; change to 20px*/
+		    100% { bottom: -150px; }
+		}
+
+	</style>
+	";
+}
+
+add_action( 'admin_head', 'pratchett_css' );
+
+?>
